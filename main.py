2d85c5b34b5842ae15df39f8fb13f8c77e095d42
Bjohnson08021 <84213057+Bjohnson08021@users.noreply.github.com>
2021-06-22 09:43:36 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,78 +1,90 @@
-
 import os
 import csv
 
-csvpath = os.path.join('.', 'Resources', 'budget_Data.csv')
- 
-total_months = 1
-sum_change = 0 
-
-
+csvpath = os.path.join('.' ,'Resources', 'election_data.csv')
+ #setting variable
+total_votes = 0
+Khan = 0
+Correy = 0
+Li = 0
+OToole = 0
 
 with open(csvpath) as csvfile:
 
-   # csv_reader = csv.reader(csv_file) use delimiter to split ,
+   # csv_reader = csv.reader(csv_file)
     csvreader= csv.reader(csvfile, delimiter=',')
     next(csvreader, None)
-    row = next(csvreader)
-#setting the variables
-    total_amount = int(row[1])
-    opening_profit = int(row[1])
-    last_month = opening_profit
-    greatest_increase = int(row[1])
-    greatest_increase_date = row[0]
-    greatest_decrease = int(row[1])
-    greatest_decrease_date = row[0]
-
-
+    
+    
+    
+    #Candidate vote count
     for row in csvreader:
-       #setting loops for these variables
-        total_months = total_months + 1
-        opening_profit = int(row[1])
-        total_amount = total_amount + opening_profit
-       #setting profit change
-        change = opening_profit - last_month
-        sum_change = sum_change + change
-        average_change = sum_change/(total_months-1)
-       #The overall greatest increase in profit with date 
-        if change > greatest_increase:
-            greatest_increase = change
-            greatest_increase_date = row [0]
-       #The overall greatest decrease in profit with date
-        if change < greatest_decrease:
-            greatest_decrease = change 
-            greatest_decrease_date = row[0]
+        
+        total_votes = total_votes + 1
+        if (row[2]) == ("Khan"):
+            Khan = Khan + 1
+        elif (row[2]) == ("Correy"):
+            Correy = Correy + 1
+        elif (row[2]) == ("Li"):
+            Li = Li + 1
+        else:
+            OToole = OToole + 1
+#Getting the percentage
+    Khan_total = Khan / total_votes * 100
+    Correy_total = Correy / total_votes * 100
+    Li_total = Li / total_votes * 100
+    OToole_total = OToole / total_votes * 100
 
-        last_month = opening_profit
+#using condtions to determine the winner
+if Khan > Correy and Khan > Li and Khan > OToole:
+    Winner = "Khan"
 
-        print("Financial Analysis")
-        print("Total Months:" + str(total_months))
-        print("Total amount:" + "$" + str(total_amount))
-        print(f"Average Change: $ {round(average_change,2)}")
-        print(f"Greatest Increase: {greatest_increase_date} ${greatest_increase}")
-        print(f"Greatest Decrease: {greatest_decrease_date} ${greatest_decrease}")
+elif Correy > Khan and Correy > Li and Correy > OToole:
+    Winner = "Correy" 
 
+elif Li > Khan and Li > Correy and Li > OToole:
+    Winner = "Li"    
 
+elif OToole > Khan and OToole > Correy and OToole > Li:
+    Winner = OToole
+        #`Candidate_list = Candidate_list + 1
+#print statement and percentage
+print(f"Election Resluts") 
+print(f"-------------------")
+print(f"Total votes: {total_votes}")
+print(f"---------------------------")
+print(f"Khan: {round(Khan_total,2)}% ({Khan})")
+print(f"Correy: {round(Correy_total,2)}% ({Correy})")
+print(f"Li: {round(Li_total,2)}% ({Li})")
+print(f"OToole: {round(OToole_total,2)}% ({OToole})")
+print("-------------------------------------")
+print(f"Winner: {Winner}")
+print("-------------------------------------")      
 
-output_path =("budget.txt")
+output_path =("election.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w', newline='') as text_file:
-    
-    
-    # Write the first row (column headers)
-    text_file.write("Financial Analysis")
+with open(output_path, 'w', newline='') as text_file: 
+
+
+    text_file.write("Election Results")
     text_file.write('\n')
     text_file.write("-------------------------------------")
     text_file.write('\n')
-    text_file.write("Total Months:" + str(total_months))
+    text_file.write(f"Total votes: {total_votes}")
     text_file.write('\n')
-    text_file.write("Total amount:" + "$" + str(total_amount))
+    text_file.write("-------------------------------------")
     text_file.write('\n')
-    text_file.write(f"Average Change: $ {round(average_change,2)}")
+    text_file.write(f"Khan: {round(Khan_total,2)}% ({Khan})")
     text_file.write('\n')
-    text_file.write(f"Greatest Increase: {greatest_increase_date} ${greatest_increase}")
+    text_file.write(f"Correy: {round(Correy_total,2)}% ({Correy})")
     text_file.write('\n')
-    text_file.write(f"Greatest Decrease: {greatest_decrease_date} ${greatest_decrease}")
-    
-    
+    text_file.write(f"Li: {round(Li_total,2)}% ({Li})")
+    text_file.write('\n')
+    text_file.write(f"OToole: {round(OToole_total,2)}% ({OToole})")
+    text_file.write('\n')
+    text_file.write("-------------------------------------")
+    text_file.write('\n')
+    text_file.write(f"Winner: {Winner}")
+    text_file.write('\n')
+    text_file.write("-------------------------------------")
